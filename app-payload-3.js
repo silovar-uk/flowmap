458fba5c9ec67765119760ce7665084dc5f656cc
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-18 22:18:17 +0900

    Remove obsolete compatibility marker

--- app-payload-3.js
+++
@@ -1,6 +0,0 @@
-/* Flowmap v0.4 compatibility marker */
-globalThis.__FLOWMAP_APP__ = "";
-const versionBadge = document.querySelector('.version-badge');
-if (versionBadge) versionBadge.textContent = 'v0.4';
-const brandIcon = document.querySelector('.brand img');
-if (brandIcon) brandIcon.src = './favicon.svg?v=0.4.0';
